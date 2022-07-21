7b4bf6b1ed79ce68d3f1d1a6ed062c5b19b3b30e
nekolr <excalibll@163.com>
2022-07-21 14:28:59 +0800

    chore: format

--- src/main/java/com/github/nekolr/peashooter/controller/req/datasource/TestRegexp.java
+++ src/main/java/com/github/nekolr/peashooter/controller/req/datasource/TestRegexp.java
@@ -2,6 +2,6 @@ package com.github.nekolr.peashooter.controller.req.datasource;
 
 import com.github.nekolr.peashooter.rss.convert.Matcher;
 
-public record TestRegexp(Matcher matcher, String [] dataSourceIds, String series, String quality, String language) {
+public record TestRegexp(Matcher matcher, String[] dataSourceIds, String series, String quality, String language) {
 
 }
