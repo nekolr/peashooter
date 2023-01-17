10e483ae8374a9776912e695467927d0d33a9ef5
nekolr <excalibll@163.com>
2023-01-17 18:11:29 +0800

    cors config

--- src/main/java/com/github/nekolr/peashooter/config/WebResourceConfig.java
+++ src/main/java/com/github/nekolr/peashooter/config/WebResourceConfig.java
@@ -1,15 +1,29 @@
 package com.github.nekolr.peashooter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebResourceConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowOrigin}")
+    private String allowOrigin;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 静态资源映射
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowOrigin)
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
