f7cbfb6f47245aff92b7283f04a7a14f4d52ce92
nekolr <excalibll@163.com>
2023-07-25 15:00:33 +0800

    update deprecated api

--- src/main/java/com/github/nekolr/peashooter/security/SecurityConfiguration.java
+++ src/main/java/com/github/nekolr/peashooter/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,65 +22,50 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                // 关闭登出
-                .logout().disable()
+
                 // 关闭 csrf
-                .csrf().disable()
+                .csrf(configurer -> configurer.disable())
 
                 // X-Frame-Options: SAMEORIGIN
-                .headers()
-                .frameOptions()
-                .sameOrigin()
+                .headers(configurer -> configurer.frameOptions(config -> config.sameOrigin()))
 
                 // X-Content-Type-Options: nosniff
-                .and()
-                .headers()
-                .contentTypeOptions()
+                .headers(configurer -> configurer.contentTypeOptions(Customizer.withDefaults()))
 
                 // X-XSS-Protection: 1; mode=block
-                .and().and()
-                .headers()
-                .xssProtection()
-                .headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK)
+                .headers(configurer -> configurer.xssProtection(config -> config.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK)))
 
                 // 授权异常处理
-                .and().and()
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint)
+                .exceptionHandling(configurer -> configurer.authenticationEntryPoint(authenticationEntryPoint))
 
                 // 不需要 session
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 // 过滤请求
-                .and()
-                .authorizeHttpRequests()
-
-                // OPTIONS 预检请求可以匿名访问
-                .requestMatchers(HttpMethod.OPTIONS, "/**").anonymous()
-                // 主页可以匿名访问
-                .requestMatchers(HttpMethod.GET, "/").anonymous()
-                .requestMatchers(HttpMethod.GET, "/index.html").anonymous()
-                // 登录请求不拦截（如果登录请求头包含 Authorization: Bearer 任意字符，那么还是会进行校验）
-                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
-                // 种子下载不拦截
-                .requestMatchers(HttpMethod.GET, "/api/torrents").permitAll()
-                // 静态资源可以匿名访问
-                .requestMatchers(HttpMethod.GET, "/assets/**").anonymous()
-                .requestMatchers(HttpMethod.GET, "/resource/**").anonymous()
-                .requestMatchers(HttpMethod.GET, "/favicon.ico").anonymous()
-                .requestMatchers(HttpMethod.GET, "/favicon.png").anonymous()
-                // 前端路由可以匿名访问
-                .requestMatchers(HttpMethod.GET, "/login").anonymous()
-                .requestMatchers(HttpMethod.GET, "/home").anonymous()
-                .requestMatchers(HttpMethod.GET, "/404").anonymous()
-                .requestMatchers(HttpMethod.GET, "/system/**").anonymous()
-                .requestMatchers(HttpMethod.GET, "/group/**").anonymous()
-                .requestMatchers(HttpMethod.GET, "/datasource/**").anonymous()
-
-                // 其他所有请求都要经过验证
-                .anyRequest().authenticated();
+                .authorizeHttpRequests(configurer ->
+                        // OPTIONS 预检请求可以匿名访问
+                        configurer.requestMatchers(HttpMethod.OPTIONS, "/**").anonymous()
+                                // 主页可以匿名访问
+                                .requestMatchers(HttpMethod.GET, "/").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/index.html").anonymous()
+                                // 登录请求不拦截（如果登录请求头包含 Authorization: Bearer 任意字符，那么还是会进行校验）
+                                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                                // 种子下载不拦截
+                                .requestMatchers(HttpMethod.GET, "/api/torrents").permitAll()
+                                // 静态资源可以匿名访问
+                                .requestMatchers(HttpMethod.GET, "/assets/**").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/resource/**").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/favicon.ico").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/favicon.png").anonymous()
+                                // 前端路由可以匿名访问
+                                .requestMatchers(HttpMethod.GET, "/login").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/home").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/404").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/system/**").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/group/**").anonymous()
+                                .requestMatchers(HttpMethod.GET, "/datasource/**").anonymous()
+                                // 其他所有请求都要经过验证
+                                .anyRequest().authenticated());
 
         httpSecurity
                 // 添加登录和权限校验的两个过滤器
