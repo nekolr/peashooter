9eace5c6cbba358d3d61a540a9cfa87514b47233
nekolr <excalibll@163.com>
2025-08-06 22:01:30 +0800

    fix: stable page json

--- src/main/java/com/github/nekolr/peashooter/PeashooterApplication.java
+++ src/main/java/com/github/nekolr/peashooter/PeashooterApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableScheduling
 @EnableWebSecurity
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class, scanBasePackages = "com.github.nekolr")
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class PeashooterApplication {
 
     public static void main(String[] args) {
